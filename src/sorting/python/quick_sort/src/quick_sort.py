991a8efee01a2b106b54c7d1f98274f8a1b05a5a
JumongEric <123095010+JumongEric@users.noreply.github.com>
2024-01-19 16:50:17 +0200

    added doc string and comments

--- src/sorting/python/quick_sort/src/quick_sort.py
+++ src/sorting/python/quick_sort/src/quick_sort.py
@@ -21,18 +21,25 @@ def quick_sort(iterable):
     except TypeError:
         raise TypeError("Input must be convertible to a list")
     seq_len= len(sequence)
-    if seq_len <=1:
-        return sequence
-    else:
+    #if the list has 1 element or is empty it will be already sorted
+    if seq_len <=1:  
+        return sequence  
+    else: #Partition the array into three parts: elements less than, equal to, and greater than the pivot.
+    
         pivot= sequence[-1] # Pivot is like the base number that will be used to for comparison
         upper_items=[]
         lower_items=[]
         equal_items=[]
+    # Then we compare each element in the sequence with the pivot    
     for i in sequence:
+
+       # if the lement is less than the pivot it is added to left side of the pivot at because it is lower than the pivot, otherwise on the right which is upper_items. 
         if i < pivot:
             lower_items.append(i)
         elif i== pivot:
             equal_items.append(i)
         else:
             upper_items.append(i)
+            
+    #Recursively apply quicksort to the left and right sub-arrays then concatenate the results.
     return quick_sort(lower_items) + equal_items + quick_sort(upper_items)
