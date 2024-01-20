d6f3ae24a798e982a293995e43c5fc17b918c7de
Vlad <35481094+Vlad421@users.noreply.github.com>
2024-01-20 11:33:28 -0700

    - ready

--- src/brain_teasers/python/array_product/src/array_product.py
+++ src/brain_teasers/python/array_product/src/array_product.py
@@ -1,7 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Python Version: 3.12
+"""
+File: array_product.py
+Author: Vlad421
+Date: 1/19/2024
 
+Description: Array product except inself function
+"""
 
 def array_product(arr:list[int]) -> list[int]:
-    
+    """Renurns new list of product of elements except itself
+
+    Parameters:
+       arr: list[int] array of int's
+
+    Returns -> list[any] product of input array except itself    
+
+    >>> array_product([1,2,3,4,5])
+    [120, 60, 40, 30, 24]
+    """
+    for each in arr:
+        assert  isinstance(each,int), "Input must be list of ontegers"
 
     new_arr:list[int] = list(arr)
     size:int = len(arr)
@@ -15,8 +35,3 @@ def array_product(arr:list[int]) -> list[int]:
     return new_arr
 
 
-
-input_array = [6]
-
-print(array_product(input_array))
-print(input_array)
