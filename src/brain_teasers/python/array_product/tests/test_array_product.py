05a695535773668a8fc5e231774b536b3654edc2
Vlad <35481094+Vlad421@users.noreply.github.com>
2024-01-20 11:42:11 -0700

    - fixed tests

--- src/brain_teasers/python/array_product/tests/test_array_product.py
+++ src/brain_teasers/python/array_product/tests/test_array_product.py
@@ -1,25 +1,33 @@
 import unittest
 
-from src.array_product import product_of_array_except_self
+import os, sys
+
+
+file_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(file_dir + "/src")
+
+
+
+from array_product import array_product
 
 
 class TestArrayProduct(unittest.TestCase):
     def test_array_product(self):
         input_array = [1, 2, 3, 4, 5]
         expected = [120, 60, 40, 30, 24]
-        actual = product_of_array_except_self(input_array)
+        actual = array_product(input_array)
         self.assertEqual(actual, expected)
 
     def test_empty_array(self):
         input_array = []
         expected = []
-        actual = product_of_array_except_self(input_array)
+        actual = array_product(input_array)
         self.assertEqual(actual, expected)
 
     def test_array_with_one_element(self):
         input_array = [1]
         expected = [1]
-        actual = product_of_array_except_self(input_array)
+        actual = array_product(input_array)
         self.assertEqual(actual, expected)
 
 
