9d9b74eecce30038edc7f70fe8fce32a8ee074ad
Vlad <35481094+Vlad421@users.noreply.github.com>
2024-01-20 16:50:38 -0700

    - done

---
+++ src/dynamic_programming/python/can_sum/README.md
@@ -0,0 +1,7 @@
+Can_sum
+Author: Vlad421
+
+Recursively determine whether elements in array can sum to target value
+- can_sum_basic() - Basic brute force algorythm
+- can_sum_memo() - Algorythm with memorization
+- can_sum_table() - Algorythm using tabulation method
--- src/dynamic_programming/python/can_sum/src/can_sum.py
+++ src/dynamic_programming/python/can_sum/src/can_sum.py
@@ -4,43 +4,78 @@
 """
 File: can_sum.py
 Author: Vlad421
-Date: 1/19/2024
-Description: Checks if array can produce given sum
+Date: 1/20/2024
+Description: Checks if array could produce given sum
 """
 
 
-def can_sum_basic(arr: list[int], number: int) -> bool:
+def can_sum_basic(number: int, arr: list[int]) -> bool:
+    """Basic brute force algorythm
 
+    Parameters:
+        arr: list[int] - List of items to sum
+        number:int - Target sum
+
+    Returns -> bool    
+
+    >>> can_sum_basic([2,1],1)
+    True """
     if number < 0:
-            return False
+        return False
     elif number == 0:
         return True
 
     for i in arr:
         reminder = number-i
-        
-        return can_sum_basic(arr[1:], reminder) or can_sum_basic(arr[1:], number)
 
-
-
-def can_sum_memo(arr: list[int], n: int, memo: int = 0) -> bool:
-
-    if memo > n:
-        return False
-    elif len(arr) > 0:
-        memo += arr[0]
-        return can_sum_memo(arr[1:], n, memo) or can_sum_memo(arr[1:], n)
-
-    elif n == memo:
-        return True
-    else:
-        return False
-
-
-def can_sum_table(arr: list[int], n: int) -> bool:
+        return can_sum_basic(reminder, arr[1:]) or can_sum_basic(number, arr[1:])
     return False
 
 
-l = [2, 1]
+def can_sum_memo(number: int, arr: list[int], __memo: int = 0) -> bool:
+    """Algorythm with memorization
 
-print(can_sum_memo(l, 1))
+    Parameters:
+        arr: list[int] - List of items to sum
+        number:int - Target sum
+        memo:int - memorization for recurcive call only
+
+    Returns -> bool    
+
+    >>> can_sum_memo([3,1,1],2)
+    True """
+
+    if __memo > number:
+        return False
+    elif number == __memo:
+        return True
+    for element in arr:
+        __memo += element
+        return can_sum_memo(number, arr[1:],  __memo) or can_sum_memo(number, arr[1:])
+
+    return False
+
+
+def can_sum_table(number: int, arr: list[int]) -> bool:
+    """Algorythm using tabulation method
+
+    Parameters:
+        arr: list[int] - List of items to sum
+        number:int - Target sum
+
+    Returns -> bool    
+
+    >>> can_sum_table([2,1,1,5],10)
+    False """
+
+    table = [False] * (number + 1)
+
+    table[0] = True
+
+    for num in arr:
+        for i in range(number, num - 1, -1):
+
+            if table[i - num]:
+                table[i] = True
+
+    return table[number]
