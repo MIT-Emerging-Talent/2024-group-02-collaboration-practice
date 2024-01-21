67583348545c35e6c102e6cd85999ebbe3347ea0
Vlad421 <35481094+Vlad421@users.noreply.github.com>
2024-01-21 12:26:59 -0700

    Update array_product.py

--- src/brain_teasers/python/array_product/src/array_product.py
+++ src/brain_teasers/python/array_product/src/array_product.py
@@ -20,17 +20,24 @@ def array_product(arr:list[int]) -> list[int]:
     >>> array_product([1,2,3,4,5])
     [120, 60, 40, 30, 24]
     """
+    # This is a for loop, checking instances
     for each in arr:
         assert  isinstance(each,int), "Input must be list of ontegers"
-
+    # This is a new List
     new_arr:list[int] = list(arr)
+    # this is a size of arr
     size:int = len(arr)
+    # another for loop
     for i in range(size):
+        # init product with one
         product:int = 1
+        # do anothe loop
         for j in range(size):
+            # check condition
             if i != j:
                 product  *= arr[j]
+        # add product to new adday
         new_arr[i] = product
-
+    # return new array
     return new_arr
 
